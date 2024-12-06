cb7531890b095aded622c7f117b4cb6e3a6db0ca
ManikantaUmmidi <ummidimanikanta484@gmail.com>
2024-12-06 23:37:04 +0530

    Added deploy steps

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Successfully Deployed to production
         </a>
       </header>
       <p>
